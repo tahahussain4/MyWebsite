f27a259e749e2d015bb6b009c37ae81c97ee7c5c
taha hussain <tahahussain@tahas-MacBook-Pro.local>
2018-08-12 22:41:11 -0400

    base href modified

--- src/app/work-experience-page/work-experience-page.component.ts
+++ src/app/work-experience-page/work-experience-page.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-work-experience-page',
   templateUrl: './work-experience-page.component.html',
   styleUrls: ['./work-experience-page.component.css']
 })
+
 export class WorkExperiencePageComponent implements OnInit {
   heading = "Work Experience";
-
   intactDescription =`I started work at intact insuarnce as part of my FDM contract in the ECM team 
   which manages two main application used by the company as well as ECM (Enterprise content management)  itself.
   Through production support I vastly improved my coding skills as well as code comprehension. I was aquainted with different technologies 
-  which were mainly based on Spring , Spring flows and Java Server Faces being two new ones I learnt. 
+  which were mainly based on Spring , Spring flow and Java Server Faces being two new ones I learnt. 
   It helped me build my team inter-team skills as I was constantly coordinating with different groups to help provide users 
   with the best service that our application had to offer thorugh coordinating releases and helping with process . 
   In addiiton to that I got opptunities to work in two projects
@@ -20,8 +21,18 @@ export class WorkExperiencePageComponent implements OnInit {
   2) Data Migration projects : Involved learning and writing DOS scritps to automate the ingesting data process`;
 
 
-  hsbcDescription = "bla bla";
-  MARCDescription = "";
+      
+
+  hsbcDescription = `I started at HSBC as part of my FDM contract . I worked on their term deposit project which 
+  focused on enhacing the exisitng applicaitons. The focus and motivation of the project was centralizing code that was used
+  for mutiple products to improve code maintainlibity as well as provide front end users a more central interface to manipulate 
+  the products. I learned to work on the AS400 in this short time and learned to code in RPG , a language specific to this 
+  platform. It was a continuous learning process in this role which I thoroughly enjoyed and had a successful end to the project`;
+
+  MARCDescription = `MARC (McMaster automotive resource centre) was a coop expereince that I took under my professor at the time.
+  The position invloved helping grad students with their research in topics. I was given the reposibility of setting up the test
+  becnh and writing C code to simulate the process that they were trying to research . The main aim was to simulate the CAN network used
+  in cars `
 
   intactExperience = new WorkExperienceObject(
   		"Intact",
@@ -40,7 +51,7 @@ export class WorkExperiencePageComponent implements OnInit {
   		"2016 Novmber",
   		"2017 May",
   		new Address("2910 Virtual Way Drive","Vancouver","BC","Canada","V5M 0B2"),
-  		this.intactDescription,
+  		this.hsbcDescription,
   		["RPG","SDLC","AS400"]
   	)
 
@@ -50,7 +61,7 @@ export class WorkExperiencePageComponent implements OnInit {
   		"2015 May",
   		"2015 September",
   		new Address("270 Lonwood Rd. South","Hamilton","ON","Canada","L8P 0A6"),
-  		this.intactDescription,
+  		this.MARCDescription,
   		["C","MicrControllers","CAN Bus","Canoe testing"]
   	)
   workExperiences = [this.intactExperience,this.hsbcExperience,this.marcExperience];
@@ -111,3 +122,6 @@ class Address{
 		this.poBox = poBox;
 	}
 }
+
+
+
