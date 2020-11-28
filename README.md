b3bde414ca9e6be40f096eb3e6f2e456a5f05875
Taha Hussain <tahahussain1994@gmail.com>
2020-11-27 20:52:32 -0500

    gettig anngular to run on heroku

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Docker command
+docker build -t mywebsite-fe:dev 
+docker run -v ${PWD}:/app -v /app/node_modules -p 4201:4200 --rm example:dev
--- server.js
+++ server.js
@@ -1,25 +1,16 @@
+//Install express server
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const http = require('http');
+
 const app = express();
 
-const api = require('./server/routes/api');
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
-
-app.use(express.static(path.join(__dirname,'dist')));
-
-app.use('/api',api);
-
-app.get("*",(req,res) => {
-	res.sendFile(path.join(__dirname,'dist/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
-const port = process.env.PORT || '3000';
-app.set('port',port)
-
-const server = http.createServer(app);
-
-server.listen(port,() => console.log('running on local host : ' + port));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
