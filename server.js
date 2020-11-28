c3f0aa3462380a8fc7ac58baeef91c8d9a4e3d24
Taha Hussain <tahahussain1994@gmail.com>
2020-11-28 00:16:35 -0500

    update

--- server.js
+++ server.js
@@ -15,4 +15,3 @@ res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-view rawgistfile1.txt hosted with ❤ by GitHub
